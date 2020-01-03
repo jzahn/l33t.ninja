b4409b6e9d5a24e8d2760656bbbd480e2f52ab85
Jason Zahn <smedge@gmail.com>
2020-01-03 16:49:59 -0600

    hide content area

---
+++ ninji.js
@@ -0,0 +1,36 @@
+logoSize = function () {
+    // Get the real width of the logo image
+    var theLogo = window.document.querySelector('#thelogo');
+    var newImage = new Image();
+    newImage.src = theLogo.attr('src');
+    var imgWidth = newImage.width;
+    
+    // distance over which zoom effect takes place
+    var maxScrollDistance = 1300;
+    
+    // set to window height if larger
+    maxScrollDistance = Math.min(maxScrollDistance, window.height());
+    
+    // width at maximum zoom out (i.e. when window has scrolled maxScrollDistance)
+    var widthAtMax = 500;
+    
+    // calculate diff and how many pixels to zoom per pixel scrolled
+    var widthDiff = imgWidth - widthAtMax;
+    var pixelsPerScroll =(widthDiff / maxScrollDistance);
+
+    // $(window).scroll(function () {
+    //     // the currently scrolled-to position - max-out at maxScrollDistance
+    //     var scrollTopPos = Math.min($(document).scrollTop(), maxScrollDistance);
+        
+    //     // how many pixels to adjust by
+    //     var scrollChangePx =  Math.floor(scrollTopPos * pixelsPerScroll);
+        
+    //     // calculate the new width
+    //     var zoomedWidth = imgWidth - scrollChangePx;
+        
+    //     // set the width
+    //     $('.logo').css('width', zoomedWidth);
+    // });
+}
+
+logoSize();
